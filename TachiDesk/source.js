c434eccec428b03f32aff1d27b29d0a447806389
AlexZorzi <alexz@europlan.it>
2026-06-08 07:07:08 +0000

    Build: (e2e43ca) Merge pull request #21 from enrico9034/fix/fetchsourcemanga-mutation
    fix: call fetchSourceManga as a mutation so browsing and search work again

--- TachiDesk/source.js
+++ TachiDesk/source.js
@@ -3600,7 +3600,7 @@ const GQL_GET_CATEGORY_MANGAS = `
     }
 `;
 const GQL_GET_SOURCE_MANGAS = `
-    query GetSourceMangas($sourceId: LongString!, $type: FetchSourceMangaType!, $page: Int!) {
+    mutation GetSourceMangas($sourceId: LongString!, $type: FetchSourceMangaType!, $page: Int!) {
         fetchSourceManga(input: { source: $sourceId, type: $type, page: $page }) {
             hasNextPage
             mangas {
@@ -3612,7 +3612,7 @@ const GQL_GET_SOURCE_MANGAS = `
     }
 `;
 const GQL_SEARCH_SOURCE = `
-    query SearchSource($sourceId: LongString!, $query: String, $page: Int!) {
+    mutation SearchSource($sourceId: LongString!, $query: String, $page: Int!) {
         fetchSourceManga(input: { source: $sourceId, type: SEARCH, page: $page, query: $query }) {
             hasNextPage
             mangas {
